145d4277ced3f7f747965cdea9f0218e5631b513
Karsten Bott <Karsten.Bott@bottnet.de>
2015-11-20 08:30:58 +0100

    Set Switchdefaults

--- labtools.psm1
+++ labtools.psm1
@@ -148,7 +148,7 @@ function Set-LABvmnet2hvswitch
     if (!(Test-Path $SwitchDefaultsfile))
     {
         Write-Warning "Creating New defaultsfile"
-        New-LABSwitchdefaults -Defaultsfile $SwitchDefaultsfile
+        New-LABSwitchdefaults -Defaultsfile .\Switchdefaultsfile.xml
     }
     $SwitchDefaults = Get-LABSwitchdefaults -SwitchDefaultsfile $SwitchDefaultsfile
     $SwitchDefaults.$($vmnet) = $hvswitch
