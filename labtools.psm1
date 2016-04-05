d7f67e857d1af9e755f034cde3bda95fa066267b
Karsten Bott <Karsten.Bott@emc.com>
2016-04-05 17:43:55 +0200

    added nx9005

--- labtools.psm1
+++ labtools.psm1
@@ -759,7 +759,8 @@ function Expand-LABZip
  [CmdletBinding(DefaultParameterSetName='Parameter Set 1',
     HelpUri = "https://github.com/bottkars/LABbuildr/wiki/LABtools#Expand-LABZip")]
 	param (
-        [Parameter(Mandatory = $true, Position = 1)][ValidateScript({ Test-Path -Path $_ -ErrorAction SilentlyContinue })][string]$zipfilename,
+        #[Parameter(Mandatory = $true, Position = 1)][ValidateScript({ Test-Path -Path $_ -ErrorAction SilentlyContinue })]
+        [string]$zipfilename,
         [string]$destination,
         [String]$Folder)
 	$copyFlag = 16 # overwrite = yes
