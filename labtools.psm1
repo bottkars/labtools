6f64b59024a14b299d376a4e07f6f8a725c6308d
Karsten Bott <Karsten.Bott@bottnet.de>
2016-11-10 05:24:40 +0100

    Possible ErrorFix

--- labtools.psm1
+++ labtools.psm1
@@ -4846,8 +4846,9 @@ if ($nodeclone.status -ne "started")
     Write-Host -ForegroundColor Cyan " ==>Testing default Route, make sure that Gateway is reachable ( eg. install and start OpenWRT )
     if failures occur, you might want to open a 2nd labbuildr windows and run start-vmx OpenWRT "
     $Scriptblock = "DEFAULT_ROUTE=`$(ip route show default | awk '/default/ {print `$3}');ping -c 1 `$DEFAULT_ROUTE"
-    Write-Verbose $Scriptblock
-    $Bashresult = $NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Guestuser $Rootuser -Guestpassword $Guestpassword     
+    $Possible_Error_Fix = "1.) If no Router is present, install OpenWRT with Receive-LAbOpenWrt -start `2.)Check if VPN Connection are Blocking inet Access for VM´s` Retry when fixed"
+	Write-Verbose $Scriptblock
+    $Bashresult = $NodeClone | Invoke-VMXBash -Scriptblock $Scriptblock -Possible_Error_Fix $Possible_Error_Fix -Guestuser $Rootuser -Guestpassword $Guestpassword     
 
 	}
 }
