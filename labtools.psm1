7b6ad3a8adc8914d5b67cc4701b07ceb077aa1d8
Karsten Bott <Karsten.Bott@bottnet.de>
2016-11-17 16:58:29 +0100

    destination

--- labtools.psm1
+++ labtools.psm1
@@ -949,33 +949,33 @@ function Expand-LABpackage
 						{
 						if ($force.ispresent)
 							{
-							$extract_Options = "-y -bb0 -bso0 -bsp0 -o"
+							$extract_Options = "-y -bb0 -bso0 -bsp0 -o" #+$destination
 							}
 						else
 							{
-							$extract_Options = "-bb0 -bso0 -bsp0 -o"
+							$extract_Options = "-bb0 -bso0 -bsp0 -o" #+$destination
 							}
 					}
 						default
 						{
 						if ($force.ispresent)
 							{
-							$extract_Options = "-y -o"
+							$extract_Options = "-y -o" #+$destination
 							}
 						else
 							{
-							$extract_Options  = "-o"
+							$extract_Options  = "-o" #+$destination
 							}
 					}
 				
 				}
 			if ($filepattern)
 				{
-				$Extract_Arguments = "$extract_Parameter $extract_Options$extract_destination $($Archive.FullName) $filepattern"
+				$Extract_Arguments = "$extract_Parameter $extract_Options$destination $($Archive.FullName) $filepattern"
 				}
 			else
 				{
-				$Extract_Arguments = "$extract_Parameter $extract_Options$extract_destination $($Archive.FullName)"
+				$Extract_Arguments = "$extract_Parameter $extract_Options$destination $($Archive.FullName)"
 				}
 			}
 		}
