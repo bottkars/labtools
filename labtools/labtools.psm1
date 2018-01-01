379a750c992b2089560cffe1549821699f5cd1bb
Karsten Bott <Karsten.Bott@emc.com>
2018-01-01 17:16:11 +0100

    fix token

--- labtools/labtools.psm1
+++ labtools/labtools.psm1
@@ -5668,7 +5668,7 @@ param(
         }
         '471'
             {
-                $Url 'https://download.microsoft.com/download/9/E/6/9E63300C-0941-4B45-A0EC-0008F96DD480/NDP471-KB4033342-x86-x64-AllOS-ENU.exe'
+                $Url  = 'https://download.microsoft.com/download/9/E/6/9E63300C-0941-4B45-A0EC-0008F96DD480/NDP471-KB4033342-x86-x64-AllOS-ENU.exe'
             }
     }
     if (Test-Path -Path "$Destination")
