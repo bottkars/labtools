0d627e324ed8c3c219cc5d0706b515d278d8e3b7
Karsten Bott <Karsten.Bott@emc.com>
2018-02-27 10:27:56 +0100

    enabled lab certs trust

--- labtools/labtools.psm1
+++ labtools/labtools.psm1
@@ -6602,6 +6602,22 @@ if ($dcvmx = get-vmx $DCNODE -WarningAction SilentlyContinue)
 }
 }
 
+function enable-labcertstrust
+{
+    Add-Type -TypeDefinition @"
+	    using System.Net;
+	    using System.Security.Cryptography.X509Certificates;
+	    public class TrustAllCertsPolicy : ICertificatePolicy {
+	        public bool CheckValidationResult(
+	            ServicePoint srvPoint, X509Certificate certificate,
+	            WebRequest request, int certificateProblem) {
+	            return true;
+	        }
+	    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object -TypeName TrustAllCertsPolicy
+
+}
 function Test-LABdcrunning
 {
 [CmdletBinding(DefaultParametersetName = "1",
